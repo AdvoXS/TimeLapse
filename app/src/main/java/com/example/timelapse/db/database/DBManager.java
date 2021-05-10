16f641398d2b16f6bfe25d3f7b57ade06b0b07f9
Ivan Borisov <tvadmix@gmail.com>
2021-05-10 15:12:37 +0300

    Поправлен менеджер базы данных

--- app/src/main/java/com/example/timelapse/db/database/DBManager.java
+++ app/src/main/java/com/example/timelapse/db/database/DBManager.java
@@ -6,14 +6,25 @@ import androidx.room.Room;
 
 public class DBManager {
 
-    public static String LOCAL_BASE = "local-base";
-    public static String ONLINE_BASE = "online-base";
+    public final static String LOCAL_BASE = "local-base";
+    public final static String ONLINE_BASE = "online-base";
+
+    public static String CURRENT_DB = null;
 
     private static AbstractDataBase instance = null;
 
     public static AbstractDataBase getDB(Context context, String dbName) {
-        if (instance == null) instance = Room.databaseBuilder(context,
-                LocalBase.class, dbName).build();
+        if (instance == null)
+            applyDB(context, dbName);
+        else if (!CURRENT_DB.equals(dbName))
+            applyDB(context, dbName);
+
         return instance;
+    }
+
+    private static void applyDB(Context context, String dbName) {
+        instance = Room.databaseBuilder(context,
+                LocalBase.class, dbName).build();
+        CURRENT_DB = dbName;
     }
 }
